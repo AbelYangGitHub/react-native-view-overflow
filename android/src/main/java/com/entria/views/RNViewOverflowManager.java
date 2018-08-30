171ef94e4d8cb24ba2905bb659e3b54122cd9a2d
杨硕 <649280333@qq.com>
2018-08-30 11:20:51 +0800

    Update RNViewOverflowManager.java

--- android/src/main/java/com/entria/views/RNViewOverflowManager.java
+++ android/src/main/java/com/entria/views/RNViewOverflowManager.java
@@ -2,10 +2,16 @@ package com.entria.views;
 
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class RNViewOverflowManager extends ViewGroupManager<RNViewOverflowLayout> {
 
     public static final String REACT_CLASS = "RNViewOverflow";
+
+    @ReactProp(name = "isClipChildren", defaultBoolean = false)
+    public void setIsClipChildren(RNViewOverflowLayout rnViewOverflowLayout, Boolean prop) {
+        rnViewOverflowLayout.setIsClipChildren(prop);
+    }
 
     @Override
      protected RNViewOverflowLayout createViewInstance(ThemedReactContext reactContext) {
